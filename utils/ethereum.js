e53a6dd48323489916d12cc7fb57abe2460fe57a
Tyler Sorensen <tyler@makerdao.com>
2019-06-17 16:40:59 +0200

    start work on integrating dai.js and connecting via metamask

---
+++ utils/ethereum.js
@@ -0,0 +1,12 @@
+export async function checkEthereumProvider() {
+  return new Promise(async (res, rej) => {
+    if (typeof window.ethereum !== 'undefined') {
+      await window.ethereum.enable();
+      const { selectedAddress, networkVersion } = window.ethereum;
+      res({
+        networkId: parseInt(networkVersion, 10),
+        address: selectedAddress
+      });
+    } else rej('No web3 provider detected');
+  });
+}
