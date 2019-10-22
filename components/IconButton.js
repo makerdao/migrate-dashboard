4670efa125b1a2a56439521d3a3ebff468f9c994
Lawrence Wang <l@lw.io>
2019-10-22 16:41:54 -0700

    fix forwardRef warning

--- components/IconButton.js
+++ components/IconButton.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { Button, Flex, Text } from '@makerdao/ui-components-core';
 
-const IconButton = ({ icon, children, ...props }) => {
+const IconButton = forwardRef(({ icon, children, ...props }, ref) => {
   return (
-    <Button variant="secondary-outline" width="22.5rem" {...props}>
+    <Button ref={ref} variant="secondary-outline" width="22.5rem" {...props}>
       <Flex alignItems="center">
         {icon}
         <Text.span m="auto" color="steel">{children}</Text.span>
       </Flex>
     </Button>
   );
-};
+});
 
 export default IconButton;
