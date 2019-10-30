d535c3537055e1c6e9022f8c94a7c81ec22ac54d
Lawrence Wang <l@lw.io>
2019-10-30 15:52:47 -0700

    remove extra stuff from landing page

--- pages/index.js
+++ pages/index.js
@@ -93,74 +93,13 @@ function Index() {
         >
           <Text.h1 mb="s">Migrate</Text.h1>
           <Breakout>
-            Upgrade your DAI, MKR, and CDPs to their respective updated
-            versions, and claims funds after emergency shutdown and other system
-            updates.
+            Use Migrate after system updates to change your Dai and CDPs into
+            their new Multi-Collateral versions. Connect your wallet to view
+            available migrations for your account.
           </Breakout>
         </Box>
         <WalletManager />
       </Grid>
-      <Box bg="white" flexGrow="1">
-        <Box maxWidth="113.4rem" width="100%" m="0 auto" px="m" mt="xl">
-          <Grid
-            gridTemplateColumns={{ s: '1fr', l: 'repeat(3, 1fr)' }}
-            gridColumnGap="2xl"
-            gridRowGap="xl"
-          >
-            <Feature
-              title="Migrate CDPs"
-              body="During a system upgrade, migrate CDPs to move them into the new system. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-            />
-            <Feature
-              title="Redeem DAI & MKR"
-              body="During a system upgrade, migrate CDPs to move them into the new system. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-            />
-            <Feature
-              title="Emergency Shutdown"
-              body="During a system upgrade, migrate CDPs to move them into the new system. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor."
-            />
-          </Grid>
-        </Box>
-
-        <Box mt="2xl" maxWidth="113.4rem" width="100%" m="0 auto" px="m">
-          <Box borderBottom={`2px solid #E1E1E1`}>
-            <Text.h5
-              color="darkPurple"
-              fontWeight="normal"
-              fontSize="1.8rem"
-              letterSpacing="-0.12px"
-              mb="s"
-            >
-              Recent additions
-            </Text.h5>
-          </Box>
-          <DatedInfo
-            date="MARCH 13, 2019"
-            title="DSChief MKR Withdrawal"
-            body="Due to the recent discovery of a potential exploit in the Maker Governance Contract (DSChief), all users are requested to withdraw any MKR deposited into one of the voting contracts back to their wallet."
-          />
-          <DatedInfo
-            date="MARCH 13, 2019"
-            title="DSChief MKR Withdrawal"
-            body="Due to the recent discovery of a potential exploit in the Maker Governance Contract (DSChief), all users are requested to withdraw any MKR deposited into one of the voting contracts back to their wallet."
-          />
-          <DatedInfo
-            date="MARCH 13, 2019"
-            title="DSChief MKR Withdrawal"
-            body="Due to the recent discovery of a potential exploit in the Maker Governance Contract (DSChief), all users are requested to withdraw any MKR deposited into one of the voting contracts back to their wallet."
-          />
-          <DatedInfo
-            date="MARCH 13, 2019"
-            title="DSChief MKR Withdrawal"
-            body="Due to the recent discovery of a potential exploit in the Maker Governance Contract (DSChief), all users are requested to withdraw any MKR deposited into one of the voting contracts back to their wallet."
-          />
-          <DatedInfo
-            date="MARCH 13, 2019"
-            title="DSChief MKR Withdrawal"
-            body="Due to the recent discovery of a potential exploit in the Maker Governance Contract (DSChief), all users are requested to withdraw any MKR deposited into one of the voting contracts back to their wallet."
-          />
-        </Box>
-      </Box>
       <Footer />
     </Flex>
   );
