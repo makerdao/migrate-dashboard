5be459d4ca28eb0342829657e943de6d8ad84813
Lawrence Wang <l@lw.io>
2019-11-12 17:47:40 -0800

    run prettier

--- components/migratesai/SCDRedeem.js
+++ components/migratesai/SCDRedeem.js
@@ -13,11 +13,13 @@ import useMaker from '../../hooks/useMaker';
 import useValidatedInput from '../../hooks/useValidatedInput';
 import { TextBlock } from '../Typography';
 
-
 export default ({ onNext, onPrev }) => {
   const [{ saiBalance }] = useStore();
   const [maxLiquidity, setMaxLiquidity] = useState(null);
-  const maxOverall = Math.min(saiBalance && saiBalance.toNumber(), maxLiquidity);
+  const maxOverall = Math.min(
+    saiBalance && saiBalance.toNumber(),
+    maxLiquidity
+  );
   const [amount, setAmount, onAmountChange, amountErrors] = useValidatedInput(
     '',
     {
@@ -56,15 +58,13 @@ export default ({ onNext, onPrev }) => {
 
   return (
     <Grid maxWidth="912px" gridRowGap="m" px={['s', 0]}>
-      <Text.h2 textAlign="center">
-        Upgrade Single Collateral Dai
-      </Text.h2>
+      <Text.h2 textAlign="center">Upgrade Single Collateral Dai</Text.h2>
       <Text.p
         textAlign="center"
         t="body"
         fontSize="1.8rem"
         m="0 auto"
-        display={{ s: "none", m: 'block' }}
+        display={{ s: 'none', m: 'block' }}
       >
         How much Single-collateral Sai would you like to upgrade to
         Multi-collateral Dai?
@@ -72,56 +72,62 @@ export default ({ onNext, onPrev }) => {
       <Grid
         gridTemplateColumns={{ s: 'minmax(0, 1fr)', l: '2fr 1fr' }}
         gridGap="m"
-        my={{ s: "s", l: "l"}}
+        my={{ s: 's', l: 'l' }}
       >
-      <Card px={{ s: 'm', m: 'l' }} py={{ s: 'm', m: 'l' }}>
-	      <Grid gridRowGap="s">
-	        <TextBlock t="h5" lineHeight="normal">Enter the amount you would like to upgrade.</TextBlock>
-	        <Input
-	          type="number"
-	          value={amount}
-	          disabled={!saiBalance}
-	          min="0"
-	          placeholder="0.00 SAI"
-	          onChange={onAmountChange}
-	          failureMessage={amountErrors}
-	          after={
-	            <Link
-	              fontWeight="medium"
-	              onClick={() => setAmount(maxOverall)}
-	            >
-	              Set max
-	            </Link>
-	          }
-	        />
-	        <Grid gridRowGap="xs">
-	        	<Box>
-			        <Text t="subheading">Sai Balance</Text>
-			        <Text
-				      	t="caption"
-				         display="inline-block"
-				         ml="s"
-				         color="darkLavender">
-			          {saiBalance ? `${saiBalance.toNumber().toFixed(2)} SAI` : '...'}
-			        </Text>
-			    </Box>
-		    </Grid>
-	      </Grid>
-      </Card>
-      <Card px={{ s: 'm', m: 'l' }} py={{ s: 'm', m: 'l' }}>
-	      <Grid gridRowGap="m">
-		      <Grid gridRowGap="xs">
-		        <TextBlock t="h5" lineHeight="normal">SAI to DAI exchange rate</TextBlock>
-		        <TextBlock t="body">1:1</TextBlock>
-		      </Grid>
-		      <Grid gridRowGap="xs">
-		        <TextBlock t="h5" lineHeight="normal">Max SAI to DAI availability</TextBlock>
-		        <TextBlock t="body">
-		          {maxLiquidity ? `${maxLiquidity.toFixed(2)} Dai` : '...'}
-		        </TextBlock>
-		      </Grid>
-	      </Grid>
-      </Card>
+        <Card px={{ s: 'm', m: 'l' }} py={{ s: 'm', m: 'l' }}>
+          <Grid gridRowGap="s">
+            <TextBlock t="h5" lineHeight="normal">
+              Enter the amount you would like to upgrade.
+            </TextBlock>
+            <Input
+              type="number"
+              value={amount}
+              disabled={!saiBalance}
+              min="0"
+              placeholder="0.00 SAI"
+              onChange={onAmountChange}
+              failureMessage={amountErrors}
+              after={
+                <Link fontWeight="medium" onClick={() => setAmount(maxOverall)}>
+                  Set max
+                </Link>
+              }
+            />
+            <Grid gridRowGap="xs">
+              <Box>
+                <Text t="subheading">Sai Balance</Text>
+                <Text
+                  t="caption"
+                  display="inline-block"
+                  ml="s"
+                  color="darkLavender"
+                >
+                  {saiBalance
+                    ? `${saiBalance.toNumber().toFixed(2)} SAI`
+                    : '...'}
+                </Text>
+              </Box>
+            </Grid>
+          </Grid>
+        </Card>
+        <Card px={{ s: 'm', m: 'l' }} py={{ s: 'm', m: 'l' }}>
+          <Grid gridRowGap="m">
+            <Grid gridRowGap="xs">
+              <TextBlock t="h5" lineHeight="normal">
+                SAI to DAI exchange rate
+              </TextBlock>
+              <TextBlock t="body">1:1</TextBlock>
+            </Grid>
+            <Grid gridRowGap="xs">
+              <TextBlock t="h5" lineHeight="normal">
+                Max SAI to DAI availability
+              </TextBlock>
+              <TextBlock t="body">
+                {maxLiquidity ? `${maxLiquidity.toFixed(2)} Dai` : '...'}
+              </TextBlock>
+            </Grid>
+          </Grid>
+        </Card>
       </Grid>
       <Grid
         justifySelf="center"
