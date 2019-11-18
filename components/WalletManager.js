55cac94b61735a1dcb0018852a3935149367a9f3
Adrian le Bas <adrianleb@gmail.com>
2019-11-18 13:50:13 +0100

    Temporarily remove ledger and trezor buttons while debugging their implementation

--- components/WalletManager.js
+++ components/WalletManager.js
@@ -3,15 +3,12 @@ import useMaker from '../hooks/useMaker';
 import IconButton from '../components/IconButton';
 import { Grid } from '@makerdao/ui-components-core';
 import BrowserProviderButton from '../components/BrowserProviderButton';
-import Trezor from '../assets/icons/trezor.svg';
-import Ledger from '../assets/icons/ledger.svg';
 import walletConnect from '../assets/icons/walletConnect.svg';
 import walletLink from '../assets/icons/wallet-link.svg';
 import { BrowserView } from 'react-device-detect';
 
 import Router from 'next/router';
 import lang from '../languages';
-import { useLedger, useTrezor } from '../hooks/useHardwareWallet';
 import { connectBrowserProvider } from '../maker';
 
 function WalletManager({ providerName }) {
@@ -33,8 +30,6 @@ function WalletManager({ providerName }) {
     Router.push('/overview');
   };
 
-  const { connectTrezorWallet } = useTrezor({ onAccountChosen });
-  const { connectLedgerWallet } = useLedger({ onAccountChosen });
 
   async function connectBrowserWallet() {
     try {
@@ -52,35 +47,6 @@ function WalletManager({ providerName }) {
         disabled={!maker}
         provider={providerName}
       />
-      <BrowserView>
-        <IconButton
-          onClick={connectTrezorWallet}
-          disabled={!maker}
-          icon={
-            <img
-              src={Trezor}
-              css={{ marginTop: -5, marginBottom: -5, paddingLeft: 5 }}
-            />
-          }
-        >
-          {lang.providers.trezor}
-        </IconButton>
-      </BrowserView>
-
-      <BrowserView>
-        <IconButton
-          onClick={connectLedgerWallet}
-          disabled={!maker}
-          icon={
-            <img
-              src={Ledger}
-              css={{ marginTop: -5, marginBottom: -5, paddingLeft: 5 }}
-            />
-          }
-        >
-          {lang.providers.ledger_nano}
-        </IconButton>
-      </BrowserView>
       <BrowserView>
         <IconButton
           onClick={() => {
