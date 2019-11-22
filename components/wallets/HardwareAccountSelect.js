4ad2a194d9982364ca0974f922138ac46847b31d
Tyler Sorensen <tyler@makerdao.com>
2019-11-22 14:50:28 -0800

    show HW wallet connection instructions while trying to load addresses

--- components/wallets/HardwareAccountSelect.js
+++ components/wallets/HardwareAccountSelect.js
@@ -15,9 +15,91 @@ import { CopyBtn, CopyBtnIcon } from './AddressTable';
 import Cross from '../../assets/icons/cross.svg';
 import { getColor } from '../../utils/theme';
 import { AccountTypes } from '../../utils/constants';
+import styled from 'styled-components';
 
 const ACCOUNTS_PER_PAGE = 5;
 const ACCOUNTS_TO_FETCH = 25;
+
+const ListContainer = styled.div`
+  margin-top: 33px;
+  margin-left: 32px;
+  margin-right: 32px;
+`;
+
+const ListItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  align-items: flex-start;
+  margin-bottom: 18px;
+`;
+
+const CircleNumber = styled.div`
+  width: 32px;
+  min-width: 32px;
+  line-height: 28px;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 16px;
+  border: 2px solid #30bd9f;
+  margin-right: 18px;
+  margin-top: 4px;
+`;
+
+const ListText = styled.div`
+  margin-top: 5px;
+  margin-right: 16px;
+  flex-grow: 1;
+  line-height: 25px;
+  font-size: 17px;
+  color: #868997;
+`;
+
+const LedgerLoading = () => (
+  <ListContainer>
+    <ListItem>
+      <CircleNumber>1</CircleNumber>
+      <ListText>Connect your Ledger to begin</ListText>
+    </ListItem>
+    <ListItem>
+      <CircleNumber>2</CircleNumber>
+      <ListText>Open the Ethereum app on the Ledger</ListText>
+    </ListItem>
+    <ListItem>
+      <CircleNumber>3</CircleNumber>
+      <ListText>
+        Ensure the Browser Support and Contract Data is enabled in Settings
+      </ListText>
+    </ListItem>
+    <ListItem>
+      <CircleNumber>4</CircleNumber>
+      <ListText>
+        You may need to update the firmware if Browser Support is not available
+      </ListText>
+    </ListItem>
+  </ListContainer>
+);
+
+const TrezorLoading = () => (
+  <ListContainer>
+    <ListItem>
+      <CircleNumber>1</CircleNumber>
+      <ListText>Connect your TREZOR Wallet to begin</ListText>
+    </ListItem>
+    <ListItem>
+      <CircleNumber>2</CircleNumber>
+      <ListText>
+        When the popop asks if you want to export the public key, select export
+      </ListText>
+    </ListItem>
+    <ListItem>
+      <CircleNumber>3</CircleNumber>
+      <ListText>
+        If required, enter your pin or password to unlock the TREZOR
+      </ListText>
+    </ListItem>
+  </ListContainer>
+);
 
 function HardwareAccountSelect({ type, path, onClose, confirmAddress }) {
   const [page, setPage] = useState(0);
@@ -57,7 +139,20 @@ function HardwareAccountSelect({ type, path, onClose, confirmAddress }) {
   const renderedAccounts = accounts.slice(start, start + ACCOUNTS_PER_PAGE);
 
   return !renderedAccounts.length ? (
-    <Loader size="5rem" color={getColor('makerTeal')} />
+    <Grid gridRowGap="m" width={['100%', '53rem']}>
+      <Flex justifyContent="flex-end">
+        <Box onClick={onClose} css={{ cursor: 'pointer' }}>
+          <img src={Cross} />
+        </Box>
+      </Flex>
+      <Grid gridRowGap="s">
+        <Text.h3 textAlign="center">Connect your {type} wallet</Text.h3>
+      </Grid>
+      {type === AccountTypes.LEDGER ? <LedgerLoading /> : <TrezorLoading />}
+      <Flex justifyContent="center">
+        <Loader size="5rem" color={getColor('makerTeal')} mb="l"/>
+      </Flex>
+    </Grid>
   ) : (
     <Grid gridRowGap="m" width={['100%', '53rem']}>
       <Flex justifyContent="flex-end">
