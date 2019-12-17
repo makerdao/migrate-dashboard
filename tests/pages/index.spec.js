8897ff0fd8b685da514dcf9ff33e091f0b6563a5
Tyler Sorensen <tyler@makerdao.com>
2019-12-17 15:57:13 -0800

    first trezor test in progress

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -1,7 +1,25 @@
 import Index from '../../pages';
 import render from '../helpers/render';
+import {
+  fireEvent,
+  wait,
+} from '@testing-library/react';
+const { click } = fireEvent;
 
 test('basic rendering', async () => {
   const { getByText } = await render(<Index />);
   getByText(/Migrate and Upgrade/);
+});
+
+test('select trezor', async () => {
+  let maker;
+  const { getByText } = await render(<Index />,{
+    getMaker: m => {
+      maker = m;
+    }
+  });
+  await wait(() => expect(maker).toBeTruthy());
+  click(getByText(/Trezor/));
+  //TODO: ^this is throwing Error: Target container is not a DOM element
+  getByText(/Connect your trezor wallet/);
 });
