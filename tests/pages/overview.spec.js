c55b3b85cc06fe3ccf38a428fe1af36b5f629218
Lawrence Wang <l@lw.io>
2019-12-18 09:25:32 -0800

    fix a test

--- tests/pages/overview.spec.js
+++ tests/pages/overview.spec.js
@@ -12,6 +12,9 @@ test('render and run all checks', async () => {
   getByText(/Migrate and Upgrade/);
   await wait(() => assert(window.maker.currentAddress()));
 
-  await waitForElement(() => getByText(/There are no migrations/));
+  // the old MKR migration should appear, because the testchain snapshot sets up
+  // the default account with old MKR:
+  // https://github.com/makerdao/testchain/blob/dai.js/scripts/deploy-scd#L154
+  await waitForElement(() => getByText('Redeem New MKR'));
   expect(window.maker.service('cdp').getCdpIds).toBeCalled();
 });
