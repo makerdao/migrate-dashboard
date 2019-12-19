d08115399da765f8b2057699c61a5a17d8e92e1d
Tyler Sorensen <tyler@makerdao.com>
2019-12-19 15:00:03 -0800

    handle and test window.open

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -11,7 +11,7 @@ test('basic rendering', async () => {
   getByText(/Migrate and Upgrade/);
 });
 
-test('select trezor', async () => {
+test('select trezor address', async () => {
   let maker;
   const { getByText } = await render(<Index />,{
     getMaker: m => {
@@ -21,7 +21,11 @@ test('select trezor', async () => {
   const div = document.createElement('DIV');
   div.id = 'modal';
   document.body.appendChild(div);
+  const mockOpen = jest.fn();
+  window.open = mockOpen;
   await wait(() => expect(maker).toBeTruthy());
   click(getByText(/Trezor/));
   getByText(/Connect your trezor wallet/);
+  await wait(() => expect(mockOpen.mock.calls.length).toBe(1));
+  //mock the trezor call to return a list of fake addresses?
 });
