84a37df418e84627dd5119627eb144be27314852
Tyler Sorensen <tyler@makerdao.com>
2019-12-19 15:13:32 -0800

    Mig 68 start of hw wallet tests (#31)

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -1,7 +1,11 @@
 import Index from '../../pages';
 import render from '../helpers/render';
+import {
+  fireEvent,
+  wait,
+} from '@testing-library/react';
+const { click } = fireEvent;
 import assert from 'assert';
-import { wait } from '@testing-library/react';
 
 test('render and get starting data', async () => {
   let state;
@@ -13,4 +17,23 @@ test('render and get starting data', async () => {
   getByText(/Migrate and Upgrade/);
   await wait(() => assert(state.saiAvailable));
   getByText(/Sai available for CDP migration/);
+});
+
+test('select trezor address', async () => {
+  let maker;
+  const { getByText } = await render(<Index />,{
+    getMaker: m => {
+      maker = m;
+    }
+  });
+  const div = document.createElement('DIV');
+  div.id = 'modal';
+  document.body.appendChild(div);
+  const mockOpen = jest.fn();
+  window.open = mockOpen;
+  await wait(() => expect(maker).toBeTruthy());
+  click(getByText(/Trezor/));
+  getByText(/Connect your trezor wallet/);
+  await wait(() => expect(mockOpen.mock.calls.length).toBe(1));
+  //mock the trezor call to return a list of fake addresses?
 });
