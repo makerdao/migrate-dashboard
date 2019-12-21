3121c75355e198f258ff08487980a3559f05bf10
Tyler Sorensen <tyler@makerdao.com>
2019-12-20 18:08:55 -0800

    mocking HW address generation, WIP

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -7,6 +7,47 @@ import {
 const { click } = fireEvent;
 import assert from 'assert';
 
+const mockAddresses = ["0xb8925CA53B2F48743E927dC4101953159c4809c1", "0x52D62C6F521e8534D646394864f43085c4180D2b", "0x5Ee6e60F8f818909621adF6bc5DE29e470620990", "0xe5FDA1099deBC1E56bc1fac835826cc1ACd81758", "0xe05FEf9Ed154f326dDF59ac2916349B94304D90a", "0x95f509E1dAf971603eb9f442e786Bd6CD272C6Fa", "0x6abE49a3Ee7d8c4bbC06cF6fCb0e6504d3a71c66", "0xE892E4D78BB68A5118d8F984633a2a62cE919E2B", "0x7Fc5CD873ecAB1cef7C4694D7d6db645DCCa7Bf9", "0x4D83190b34982236b52a04c478a604a0Ff9d820A"];
+
+const mockHardwareFactory = jest.fn(maker => {
+  maker.service('accounts', true).addAccountType('ledger', async settings => {
+    const subprovider = {};
+
+    let address;
+
+    if (settings.accountsLength && settings.accountsLength > 1) {
+      if (!settings.choose) {
+        throw new Error(
+          'If accountsLength > 1, "choose" must be defined in account options.'
+        );
+      }
+
+      const addresses = await new Promise(resolve =>
+        setTimeout(()=>resolve(mockAddresses), 100)
+      );
+      address = await new Promise((resolve, reject) => {
+        const callback = (err, address) =>
+          err ? reject(err) : resolve(address);
+
+        // this chooser function allows the app using the plugin to display the
+        // list of addresses to a human user and let them make a choice.
+        settings.choose(
+          Object.keys(addresses).map(k => addresses[k]),
+          callback
+        );
+      });
+    } else {
+      address = await new Promise((resolve, reject) =>
+        subprovider.getAccounts((err, addresses) =>
+          err ? reject(err) : resolve(addresses[0])
+        )
+      );
+    }
+
+    return { subprovider, address };
+  });
+});
+
 test('render and get starting data', async () => {
   let state;
   const { getByText } = await render(<Index />, {
@@ -29,12 +70,11 @@ test('select trezor address', async () => {
   const div = document.createElement('DIV');
   div.id = 'modal';
   document.body.appendChild(div);
-  const mockOpen = jest.fn();
-  window.open = mockOpen;
   await wait(() => expect(maker).toBeTruthy());
+  maker.service('accounts')._accountFactories['trezor'] = mockHardwareFactory;
   click(getByText(/Trezor/));
   getByText(/Connect your trezor wallet/);
-  await wait(() => expect(mockOpen.mock.calls.length).toBe(1));
+  setTimeout(()=>getByText(/Select address/),200);
 });
 
 test('select ledger address', async () => {
