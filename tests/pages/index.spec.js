179620a24e695170c9bc95927fbe6a89d0afd144
Tyler Sorensen <tyler@makerdao.com>
2020-01-06 21:42:05 -0800

    some more progress on address selection

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -7,7 +7,7 @@ import {
 const { click } = fireEvent;
 import assert from 'assert';
 
-const mockAddresses = ["0xb8925CA53B2F48743E927dC4101953159c4809c1", "0x52D62C6F521e8534D646394864f43085c4180D2b", "0x5Ee6e60F8f818909621adF6bc5DE29e470620990", "0xe5FDA1099deBC1E56bc1fac835826cc1ACd81758", "0xe05FEf9Ed154f326dDF59ac2916349B94304D90a", "0x95f509E1dAf971603eb9f442e786Bd6CD272C6Fa", "0x6abE49a3Ee7d8c4bbC06cF6fCb0e6504d3a71c66", "0xE892E4D78BB68A5118d8F984633a2a62cE919E2B", "0x7Fc5CD873ecAB1cef7C4694D7d6db645DCCa7Bf9", "0x4D83190b34982236b52a04c478a604a0Ff9d820A"];
+const mockAddresses = ['0xb8925CA53B2F48743E927dC4101953159c4809c1', '0x52D62C6F521e8534D646394864f43085c4180D2b', '0x5Ee6e60F8f818909621adF6bc5DE29e470620990', '0xe5FDA1099deBC1E56bc1fac835826cc1ACd81758', '0xe05FEf9Ed154f326dDF59ac2916349B94304D90a', '0x95f509E1dAf971603eb9f442e786Bd6CD272C6Fa', '0x6abE49a3Ee7d8c4bbC06cF6fCb0e6504d3a71c66', '0xE892E4D78BB68A5118d8F984633a2a62cE919E2B', '0x7Fc5CD873ecAB1cef7C4694D7d6db645DCCa7Bf9', '0x4D83190b34982236b52a04c478a604a0Ff9d820A'];
 
 const mockHardwareFactory = jest.fn(maker => {
   maker.service('accounts', true).addAccountType('ledger', async settings => {
@@ -77,9 +77,9 @@ test('select trezor address', async () => {
   setTimeout(()=>getByText(/Select address/),200);
 });
 
-test('select ledger address', async () => {
+test.only('select ledger address', async done => {
   let maker;
-  const { getByText, getAllByRole } = await render(<Index />,{
+  const { getByText, getAllByText } = await render(<Index />,{
     getMaker: m => {
       maker = m;
     }
@@ -88,8 +88,15 @@ test('select ledger address', async () => {
   div.id = 'modal';
   document.body.appendChild(div);
   await wait(() => expect(maker).toBeTruthy());
+  maker.service('accounts')._accountFactories['ledger'] = mockHardwareFactory;
   click(getByText(/Ledger Nano/));
   getByText(/Connect Ledger Live or Legacy/);
-  click(getAllByRole('button')[0]);
+  click(getAllByText('Connect')[0]); //click the 1st connect button (Ledger Live)
   getByText(/Connect your ledger wallet/);
+  expect(mockHardwareFactory).toBeCalled();
+  //change back to wait, just for simplicity?
+  setTimeout(()=>{
+    getByText(/Select address/);
+    done();
+  },200);
 });
