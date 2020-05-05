a74736419d2ac2266125a30f08876a5978be581e
Ashoka Finley <ashoka.finley@example.com>
2020-05-05 14:57:18 -0400

    Reformat table to be like design

--- components/redeemdai/CollateralTable.js
+++ components/redeemdai/CollateralTable.js
@@ -1,14 +1,158 @@
 import {
   Text,
+  Box,
   Grid,
-  Table,
+  Card,
   Button,
-  Tooltip
+  Tooltip,
+  Flex
 } from '@makerdao/ui-components-core';
+import styled from 'styled-components';
+import { getColor } from '../../utils/theme';
 import TooltipContents from '../TooltipContents';
 import { prettifyNumber } from '../../utils/ui';
 import BigNumber from 'bignumber.js';
 import SuccessButton from '../SuccessButton';
+
+const TABLE_COLUMNS = '1fr 1fr 2fr 2fr 2fr 2fr';
+
+const Label = styled(Box)`
+  text-transform: uppercase;
+  font-weight: bold;
+  font-size: 13px;
+  color: ${getColor('steel')};
+`;
+
+function ListItemRow({ label, children, dark }) {
+  return (
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      bg={dark ? getColor('lightGrey') : 'white'}
+      px="m"
+      py="s"
+    >
+      <Label>{label}</Label>
+      <div>{children}</div>
+    </Flex>
+  );
+}
+
+function ListItem({
+    ilk,
+    tagData,
+    price,
+    amount,
+    maxRedeem,
+    redeemComplete,
+    buttonLoading,
+    buttonDisabled,
+    redeemDai,
+    ...otherProps
+  }) {
+  const ilkMaxRedeem = maxRedeem.find(x => x.ilk === ilk).max;
+  const token = ilk.split('-')[0];
+  const value = `$${prettifyNumber(
+    BigNumber(1).div(tagData.find(t => t.ilk === ilk).price)
+  )}`;
+  const exchangeRate = `1 DAI : ${prettifyNumber(price, false, 4)} ${token}`;
+  const redeeming = amount
+    ? `${prettifyNumber(
+      price.times(ilkMaxRedeem),
+      false,
+      price
+        .times(ilkMaxRedeem)
+        .gt(0.01)
+        ? 2
+        : 4
+      )} ${ilk.split('-')[0]}`
+    : '';
+  const redeemingDai = amount
+    ? `${prettifyNumber(
+      ilkMaxRedeem,
+      false,
+      ilkMaxRedeem.gt(0.01)
+        ? 2
+        : 4
+      )} DAI`
+    : '';
+  const redemptionButton = redeemComplete.includes(ilk) ||
+  ilkMaxRedeem.eq(0) ? (
+    <SuccessButton
+      py="4px"
+      justifySelf="center"
+
+    />
+  ) : (
+    <Button
+      py="4px"
+      px="4px"
+      mx="16px"
+      justifySelf="center"
+      width="100%"
+      fontSize={'16px'}
+      loading={buttonLoading.includes(ilk)}
+      disabled={
+        buttonDisabled ||
+        ilkMaxRedeem.eq(0)
+      }
+      onClick={() =>
+        redeemDai(maxRedeem.find(x => x.ilk === ilk).max, ilk)
+      }
+    >
+      Redeem
+    </Button>
+  );
+  return (
+    <Card
+      px={['0', 'l']}
+      py={['0', 'm']}
+      mt={{s: 'm', l: 'xs'}}
+      borderColor={'#D4D9E1'}
+      border={'1px solid'}
+      {...otherProps}
+    >
+      <Box display={['none', 'block']}>
+        <Grid
+          gridTemplateColumns={TABLE_COLUMNS}
+          gridColumnGap="l"
+          alignItems="center"
+          fontSize="m"
+          color="darkPurple"
+          css="white-space: nowrap;"
+        >
+          <span>{token}</span>
+          <span>{value}</span>
+          <span>{exchangeRate}</span>
+          <span>{redeeming}</span>
+          <span>{redeemingDai}</span>
+          {redemptionButton}
+        </Grid>
+      </Box>
+      <Box display={['block', 'none']}>
+        <ListItemRow label="Token" dark>
+          {token}
+        </ListItemRow>
+        <ListItemRow label="Value">
+          {value}
+        </ListItemRow>
+        <ListItemRow label="Exchange Rate" dark>
+          {exchangeRate}
+        </ListItemRow>
+        <ListItemRow label="Redeeming">
+          {redeeming}
+        </ListItemRow>
+        <ListItemRow label="Reedeming (DAI)" dark>
+          {redeemingDai}
+        </ListItemRow>
+        <Flex alignItems="center" justifyContent="center" my="m">
+          {redemptionButton}
+        </Flex>
+
+      </Box>
+    </Card>
+  );
+}
 
 function CollateralTable({
   data,
@@ -33,156 +177,86 @@ function CollateralTable({
       })
     : outAmounts;
   return (
-    <Grid gridRowGap="s" p="m">
-      <Table>
-        <Table.thead>
-          <Table.tr py="xl" border="0px">
-            <Table.th>
-              <Text t="subheading">Token</Text>
-              <Tooltip style={{ visibility: 'hidden' }} fontSize="m" content={<div/>} />
-            </Table.th>
-            <Table.th>
-              <Text t="subheading">Price</Text>
-              <Tooltip
-                color="steel"
-                fontSize="m"
-                ml="2xs"
-                content={
-                  <TooltipContents>
-                    Token price at time of shutdown
-                  </TooltipContents>
-                }
-              />
-            </Table.th>
-            <Table.th>
-              <Text t="subheading">Exchange Rate</Text>
-              <Tooltip
-                color="steel"
-                fontSize="m"
-                ml="2xs"
-                content={
-                  <TooltipContents>
-                    Amount of token redeemable per 1 DAI, after accounting for
-                    the token's proportion of the collateral pool (and any
-                    system surplus or deficit if applicable)
-                  </TooltipContents>
-                }
-              />
-            </Table.th>
-            <Table.th>
-              <Text t="subheading">Redeeming</Text>
-              <Tooltip
-                color="steel"
-                fontSize="m"
-                ml="2xs"
-                content={
-                  <TooltipContents>
-                    Total amount of token to be received
-                  </TooltipContents>
-                }
-              />
-            </Table.th>
-            <Table.th>
-              <Text t="subheading">Redeeming (Dai)</Text>
-              <Tooltip
-                color="steel"
-                fontSize="m"
-                ml="2xs"
-                content={
-                  <TooltipContents>Dai amount being redeemed</TooltipContents>
-                }
-              />
-            </Table.th>
-          </Table.tr>
-        </Table.thead>
-        <Table.tbody>
-          {data &&
-            data.map(({ ilk, price }, idx) => (
-              <Table.tr key={idx}>
-                <Table.td>
-                  <Text.p my="m" fontSize="1.5rem" t="body" fontWeight={400}>
-                    {ilk.split('-')[0]}
-                  </Text.p>
-                </Table.td>
+    <Grid maxWidth="912px" px={['s', 0]} justifyContent="center">
+      <Box display={['none', 'block']}>
+        <Grid
+          mt="m"
+          px="l"
+          pb="0"
+          gridTemplateColumns={TABLE_COLUMNS}
+          gridColumnGap="l"
+          alignItems="center"
+          fontWeight="medium"
+          color="steelLight"
+          css="white-space: nowrap;"
+        >
 
-                <Table.td>
-                  <Text.p my="m" fontSize="1.5rem" t="body" fontWeight={400}>
-                    {`$${prettifyNumber(
-                      BigNumber(1).div(tagData.find(t => t.ilk === ilk).price)
-                    )}`}
-                  </Text.p>
-                </Table.td>
-
-                <Table.td>
-                  <Text.p my="m" fontSize="1.5rem" t="body" fontWeight={400}>
-                    {`1 DAI : ${prettifyNumber(price, false, 4)} ${
-                      ilk.split('-')[0]
-                    }`}
-                  </Text.p>
-                </Table.td>
-
-                <Table.td>
-                  <Text.p my="m" fontSize="1.5rem" t="body" fontWeight={400}>
-                    {amount
-                      ? `${prettifyNumber(
-                          price.times(maxRedeem.find(x => x.ilk === ilk).max),
-                          false,
-                          price
-                            .times(maxRedeem.find(x => x.ilk === ilk).max)
-                            .gt(0.01)
-                            ? 2
-                            : 4
-                        )} ${ilk.split('-')[0]}`
-                      : ''}
-                  </Text.p>
-                </Table.td>
-
-                <Table.td>
-                  <Text.p my="m" fontSize="1.5rem" t="body" fontWeight={400}>
-                    {amount
-                      ? `${prettifyNumber(
-                          maxRedeem.find(x => x.ilk === ilk).max,
-                          false,
-                          maxRedeem.find(x => x.ilk === ilk).max.gt(0.01)
-                            ? 2
-                            : 4
-                        )} DAI`
-                      : ''}
-                  </Text.p>
-                </Table.td>
-                <Table.td>
-                  {redeemComplete.includes(ilk) ||
-                  maxRedeem.find(x => x.ilk === ilk).max.eq(0) ? (
-                    <SuccessButton
-                      px="16px"
-                      py="4px"
-                      width="90px"
-                      justifySelf="center"
-                    />
-                  ) : (
-                    <Button
-                      px="16px"
-                      py="4px"
-                      width="90px"
-                      justifySelf="center"
-                      fontSize={'13px'}
-                      loading={buttonLoading.includes(ilk)}
-                      disabled={
-                        buttonDisabled ||
-                        maxRedeem.find(x => x.ilk === ilk).max.eq(0)
-                      }
-                      onClick={() =>
-                        redeemDai(maxRedeem.find(x => x.ilk === ilk).max, ilk)
-                      }
-                    >
-                      Redeem
-                    </Button>
-                  )}
-                </Table.td>
-              </Table.tr>
-            ))}
-        </Table.tbody>
-      </Table>
+          <Flex flexDirection='row' alignItems="center">
+            <Text t="subheading">Token</Text>
+          </Flex>
+          <Flex flexDirection='row' alignItems="center">
+            <Text t="subheading">Value</Text>
+            <Tooltip
+              color="steel"
+              fontSize="m"
+              ml="2xs"
+              content={
+                <TooltipContents>
+                  Token price at time of shutdown
+                </TooltipContents>
+              }
+            />
+          </Flex>
+          <Flex flexDirection='row' alignItems="center">
+            <Text t="subheading">Exchange Rate</Text>
+            <Tooltip
+              color="steel"
+              fontSize="m"
+              ml="2xs"
+              content={
+                <TooltipContents>
+                  {`Amount of token redeemable per 1 DAI, after accounting for
+                  the token's proportion of the collateral pool (and any
+                  system surplus or deficit if applicable`}
+                </TooltipContents>
+              }
+            />
+          </Flex>
+          <Flex flexDirection='row' alignItems="center">
+            <Text t="subheading">Redeeming</Text>
+            <Tooltip
+              color="steel"
+              fontSize="m"
+              ml="2xs"
+              content={
+                <TooltipContents>
+                  Total amount of token to be received
+                </TooltipContents>
+              }
+            />
+          </Flex>
+          <Flex flexDirection='row' alignItems="center">
+            <Text t="subheading">Redeeming (DAI)</Text>
+            <Tooltip
+              color="steel"
+              fontSize="m"
+              ml="2xs"
+              content={
+                <TooltipContents>Dai amount being redeemed</TooltipContents>
+              }
+            />
+          </Flex>
+          <span />
+        </Grid>
+      </Box>
+      <Grid gridRowGap="s" pb="m" mt="m">
+        {data && data.map(({ ilk, price }, idx) => (
+          <ListItem
+            key={idx}
+            {...{ ilk, price, tagData, amount, maxRedeem, redeemComplete, buttonLoading, buttonDisabled, redeemDai }}
+          />
+        ))}
+      </Grid>
     </Grid>
   );
 }
